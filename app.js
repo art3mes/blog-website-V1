6586b3f621300ba1f77decdb3ce7a9b3e559f8e3
art3mes <93867313+art3mes@users.noreply.github.com>
2023-05-02 18:12:17 +0530

    composed posts are available to be viewed fomr the home page

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mongoose=require('mongoose');
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-const posts=[];
+//const posts=[];
 
 let app = express();
 
@@ -26,9 +26,12 @@ const composeSchema={
 const Post=mongoose.model('Post',composeSchema);
 
 app.get("/",function(req,res){
-  res.render("home.ejs", {
-    homeString:homeStartingContent,
-    posts:posts
+  Post.find().then(function(posts){
+
+    res.render("home.ejs", {
+      homeString:homeStartingContent,
+      posts:posts
+    }); 
   });
 });
 app.get("/about", function(req,res){
@@ -44,13 +47,17 @@ app.get("/contact", function(req,res){
 app.get('/posts/:postName', function(req, res){                 //making a dynamic website
   const requestedTitle=_.lowerCase(req.params.postName);
  // console.log(req.params);                                  //prints all parameters
-
-  posts.forEach(function(post){                             //searching for a post in already made posts
-    let storedTitle=_.lowerCase(post.title);
-    if(storedTitle===requestedTitle){
-     res.render("post.ejs",{postTitle: post.title, postContent: post.content} );     //making separate page for each post 🌟
-    }
-  });
+  Post.find().then(function(posts){
+    posts.forEach(function(per){ 
+      console.log(per.title);
+    });
+    posts.forEach(function(post){                             //searching for a post in already made posts
+      let storedTitle=_.lowerCase(post.title);
+      if(storedTitle===requestedTitle){
+      res.render("post.ejs",{postTitle: post.title, postContent: post.bodyText} );     //making separate page for each post 🌟
+      }
+    });
+  }); 
 });
 
 app.post("/compose", function(req,res){
@@ -63,7 +70,7 @@ app.post("/compose", function(req,res){
     bodyText:post.content
   });
   composedPost.save();
-  posts.push(post);
+  //posts.push(post);
  
   res.redirect("/");
 });
