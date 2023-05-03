4d4d20c39e4133a0eaccdb9dfe1d544d17cfda66
art3mes <93867313+art3mes@users.noreply.github.com>
2023-05-03 10:20:42 +0530

    finisjing up. adding some more css

--- app.js
+++ app.js
@@ -5,10 +5,9 @@ const ejs = require("ejs");
 var _ = require('lodash');                           //LODASH
 const mongoose=require('mongoose');
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-//const posts=[];
+const homeStartingContent = "Welcome to our blog! Here, we share informative and inspiring content on a variety of topics that matter to you. Whether you're looking for advice on personal growth, tips for a healthier lifestyle, or insights on the latest trends and innovations, we've got you covered. Our team of writers and experts are passionate about creating engaging and informative content that will help you navigate through life's challenges and find new opportunities for growth and success. So sit back, grab a cup of coffee, and enjoy our latest articles and insights.";
+const aboutContent = "At our website, we are dedicated to providing our readers with the most valuable and insightful content possible. Our team is made up of passionate writers, researchers, and experts who are committed to delivering high-quality articles that inform, educate, and inspire our readers. Whether you're looking for advice on personal growth, health and wellness, or the latest industry trends and news, our website is your go-to destination for all things informative and inspiring. We strive to create a welcoming and inclusive community where our readers can connect, share their experiences, and learn from each other.";
+const contactContent = "We always love to hear from our readers! If you have any questions, comments, or feedback, please don't hesitate to reach out to us. You can contact us by filling out the form on this page, and we'll do our best to get back to you as soon as possible. Alternatively, you can also connect with us on social media or send us an email. We appreciate your support and look forward to hearing from you!";
 
 let app = express();
 
@@ -48,9 +47,9 @@ app.get('/posts/:postName', function(req, res){                 //making a dynam
   const requestedTitle=_.lowerCase(req.params.postName);
  // console.log(req.params);                                  //prints all parameters
   Post.find().then(function(posts){
-    posts.forEach(function(per){ 
-      console.log(per.title);
-    });
+    // posts.forEach(function(per){                         printing post title on console
+    //   console.log(per.title);
+    // });
     posts.forEach(function(post){                             //searching for a post in already made posts
       let storedTitle=_.lowerCase(post.title);
       if(storedTitle===requestedTitle){
@@ -69,11 +68,10 @@ app.post("/compose", function(req,res){
     title:post.title,
     bodyText:post.content
   });
-  composedPost.save();
-  //posts.push(post);
- 
+  composedPost.save(); 
   res.redirect("/");
 });
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
